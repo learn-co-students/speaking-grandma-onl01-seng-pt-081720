059c2719fcd020dbbdf81902c71125e1c96afb37
Myles Janik <mylesjanik@gmail.com>
2020-08-30 00:57:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   elsif (phrase != phrase.upcase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif (phrase == phrase.upcase)
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   else
     return "ERROR"
   end
